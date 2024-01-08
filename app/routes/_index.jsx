c36e7d14249b961c6214506cecb756b4409c068c
M3rcena <michaelfarmakis2007@gmail.com>
2024-01-08 19:50:41 +0200

    Updated Links + Buttons in Home

--- app/routes/_index.jsx
+++ app/routes/_index.jsx
@@ -4,17 +4,18 @@ import { Helmet } from 'react-helmet'
 
 import { FeatureCard, links as featureCardLinks } from '../components/feature-card/feature-card'
 import { Question, links as questionLinks } from '../components/question/question'
-import homeStylesHref from "../styles/home.css"
+import indexStylesHref from "../styles/index.css"
+import { Link, useNavigate } from '@remix-run/react'
 
 export const links = () => {
   return [
     ...featureCardLinks(),
     ...questionLinks(),
-    { rel: 'stylesheet', href: homeStylesHref },
+    { rel: 'stylesheet', href: indexStylesHref },
   ]
 }
-
 export default function Home() {
+  let navigate = useNavigate()
   return (
     <>
     <div className="home-container">
@@ -27,15 +28,15 @@ export default function Home() {
           <span className="home-logo">MY PG</span>
           <div data-thq="thq-navbar-nav" className="home-desktop-menu">
             <nav className="home-links">
-              <span>Home</span>
-              <span className="home-nav2">About</span>
-              <span className="home-nav3">Features</span>
-              <span className="home-nav4">Parties</span>
-              <span className="home-nav5">Discounts</span>
+              <span><Link to="/">Home</Link></span>
+              <span className="home-nav2"><Link to="/about">About</Link></span>
+              <span className="home-nav3"><Link to="/features">Features</Link></span>
+              <span className="home-nav4"><Link to="/parties">Parties</Link></span>
+              <span className="home-nav5"><Link to="/discounts">Discounts</Link></span>
             </nav>
             <div className="home-buttons">
-              <button className="home-login button">Login</button>
-              <button className="home-register button">Register</button>
+              <button className="home-login button"><Link to="/login">Login</Link></button>
+              <button className="home-register button"><Link to="/register">Register</Link></button>
             </div>
           </div>
           <div data-thq="thq-burger-menu" className="home-burger-menu">
@@ -46,7 +47,7 @@ export default function Home() {
           <div data-thq="thq-mobile-menu" className="home-mobile-menu">
             <div className="home-nav">
               <div className="home-top">
-                <span className="home-logo1">MY PG</span>
+                <span className="home-logo1"><Link to="/">MY PG</Link></span>
                 <div data-thq="thq-close-menu" className="home-close-menu">
                   <svg viewBox="0 0 1024 1024" className="home-icon02">
                     <path d="M810 274l-238 238 238 238-60 60-238-238-238 238-60-60 238-238-238-238 60-60 238 238 238-238z"></path>
@@ -54,15 +55,15 @@ export default function Home() {
                 </div>
               </div>
               <nav className="home-links1">
-                <span className="home-nav11">Home</span>
-                <span className="home-nav21">About</span>
-                <span className="home-nav31">Features</span>
-                <span className="home-nav41">Parties</span>
-                <span className="home-nav51">Discounts</span>
+                <span className="home-nav11"><Link to="/">Home</Link></span>
+                <span className="home-nav21"><Link to="/about">About</Link></span>
+                <span className="home-nav31"><Link to="/features">Features</Link></span>
+                <span className="home-nav41"><Link to="/parties">Parties</Link></span>
+                <span className="home-nav51"><Link to="/discount">Discounts</Link></span>
               </nav>
               <div className="home-buttons1">
-                <button className="home-login1 button">Login</button>
-                <button className="home-register1 button">Register</button>
+                <button className="home-login1 button"><Link to="/login">Login</Link></button>
+                <button className="home-register1 button"><Link to="/register">Register</Link></button>
               </div>
             </div>
             <div>
@@ -89,8 +90,8 @@ export default function Home() {
               Explore your city with our interactive map
             </span>
             <div className="home-btn-group">
-              <button className="home-hero-button1 button">Get Started</button>
-              <button className="home-hero-button2 button">Learn More →</button>
+              <button className="home-hero-button1 button" onClick={() => navigate("/map")}>Get Started</button>
+              <button className="home-hero-button2 button" onClick={() => navigate("/about")}>Learn More →</button>
             </div>
           </div>
         </div>
@@ -209,7 +210,7 @@ export default function Home() {
                   </span>
                 </div>
               </div>
-              <button className="home-button button">Continue with Free</button>
+              <button className="home-button button" onClick={() => navigate('/login')}>Continue with Free</button>
             </div>
             <div className="home-pricing-card1">
               <div className="home-container14">
@@ -259,7 +260,7 @@ export default function Home() {
                   </span>
                 </div>
               </div>
-              <button className="home-button1 button">
+              <button className="home-button1 button" onClick={() => navigate("/account/basic")}>
                 Try the Basic plan
               </button>
             </div>
@@ -307,7 +308,7 @@ export default function Home() {
                   </span>
                 </div>
               </div>
-              <button className="home-button2 button">Try the PRO plan</button>
+              <button className="home-button2 button" onClick={() => navigate("/account/pro")}>Try the PRO plan</button>
             </div>
           </div>
         </div>
@@ -321,7 +322,7 @@ export default function Home() {
           <span className="home-banner-sub-heading">
             Get access to special offers and easily organize parties at parks
           </span>
-          <button className="home-banner-button button">Learn more</button>
+          <button className="home-banner-button button" onClick={() => navigate("/account/offers")}>Learn more</button>
         </div>
       </div>
       <div className="home-faq">
@@ -436,13 +437,13 @@ export default function Home() {
       <div className="home-footer">
         <footer className="home-footer1">
           <div className="home-container31">
-            <span className="home-logo2">MY PG</span>
+            <span className="home-logo2"><Link to="/">MY PG</Link></span>
             <nav className="home-nav1 home-nav1">
-              <span className="home-nav12">Home</span>
-              <span className="home-nav22">About</span>
-              <span className="home-nav32">Features</span>
-              <span className="home-nav42">Parties</span>
-              <span className="home-nav52">Discounts</span>
+              <span className="home-nav12"><Link to="/">Home</Link></span>
+              <span className="home-nav22"><Link to="/about">About</Link></span>
+              <span className="home-nav32"><Link to="/features">Features</Link></span>
+              <span className="home-nav42"><Link to="/parties">Parties</Link></span>
+              <span className="home-nav52"><Link to="/discounts">Discounts</Link></span>
             </nav>
           </div>
           <div className="home-separator"></div>
@@ -454,9 +455,11 @@ export default function Home() {
               <svg viewBox="0 0 950.8571428571428 1024" className="home-icon10">
                 <path d="M925.714 233.143c-25.143 36.571-56.571 69.143-92.571 95.429 0.571 8 0.571 16 0.571 24 0 244-185.714 525.143-525.143 525.143-104.571 0-201.714-30.286-283.429-82.857 14.857 1.714 29.143 2.286 44.571 2.286 86.286 0 165.714-29.143 229.143-78.857-81.143-1.714-149.143-54.857-172.571-128 11.429 1.714 22.857 2.857 34.857 2.857 16.571 0 33.143-2.286 48.571-6.286-84.571-17.143-148-91.429-148-181.143v-2.286c24.571 13.714 53.143 22.286 83.429 23.429-49.714-33.143-82.286-89.714-82.286-153.714 0-34.286 9.143-65.714 25.143-93.143 90.857 112 227.429 185.143 380.571 193.143-2.857-13.714-4.571-28-4.571-42.286 0-101.714 82.286-184.571 184.571-184.571 53.143 0 101.143 22.286 134.857 58.286 41.714-8 81.714-23.429 117.143-44.571-13.714 42.857-42.857 78.857-81.143 101.714 37.143-4 73.143-14.286 106.286-28.571z"></path>
               </svg>
-              <svg viewBox="0 0 877.7142857142857 1024" className="home-icon12">
-                <path d="M585.143 512c0-80.571-65.714-146.286-146.286-146.286s-146.286 65.714-146.286 146.286 65.714 146.286 146.286 146.286 146.286-65.714 146.286-146.286zM664 512c0 124.571-100.571 225.143-225.143 225.143s-225.143-100.571-225.143-225.143 100.571-225.143 225.143-225.143 225.143 100.571 225.143 225.143zM725.714 277.714c0 29.143-23.429 52.571-52.571 52.571s-52.571-23.429-52.571-52.571 23.429-52.571 52.571-52.571 52.571 23.429 52.571 52.571zM438.857 152c-64 0-201.143-5.143-258.857 17.714-20 8-34.857 17.714-50.286 33.143s-25.143 30.286-33.143 50.286c-22.857 57.714-17.714 194.857-17.714 258.857s-5.143 201.143 17.714 258.857c8 20 17.714 34.857 33.143 50.286s30.286 25.143 50.286 33.143c57.714 22.857 194.857 17.714 258.857 17.714s201.143 5.143 258.857-17.714c20-8 34.857-17.714 50.286-33.143s25.143-30.286 33.143-50.286c22.857-57.714 17.714-194.857 17.714-258.857s5.143-201.143-17.714-258.857c-8-20-17.714-34.857-33.143-50.286s-30.286-25.143-50.286-33.143c-57.714-22.857-194.857-17.714-258.857-17.714zM877.714 512c0 60.571 0.571 120.571-2.857 181.143-3.429 70.286-19.429 132.571-70.857 184s-113.714 67.429-184 70.857c-60.571 3.429-120.571 2.857-181.143 2.857s-120.571 0.571-181.143-2.857c-70.286-3.429-132.571-19.429-184-70.857s-67.429-113.714-70.857-184c-3.429-60.571-2.857-120.571-2.857-181.143s-0.571-120.571 2.857-181.143c3.429-70.286 19.429-132.571 70.857-184s113.714-67.429 184-70.857c60.571-3.429 120.571-2.857 181.143-2.857s120.571-0.571 181.143 2.857c70.286 3.429 132.571 19.429 184 70.857s67.429 113.714 70.857 184c3.429 60.571 2.857 120.571 2.857 181.143z"></path>
-              </svg>
+              <a href='https://www.instagram.com/play.ground4you?igsh=MzRlODBiNWFlZA==' target='_blank'>
+                <svg viewBox="0 0 877.7142857142857 1024" className="home-icon12">
+                  <path d="M585.143 512c0-80.571-65.714-146.286-146.286-146.286s-146.286 65.714-146.286 146.286 65.714 146.286 146.286 146.286 146.286-65.714 146.286-146.286zM664 512c0 124.571-100.571 225.143-225.143 225.143s-225.143-100.571-225.143-225.143 100.571-225.143 225.143-225.143 225.143 100.571 225.143 225.143zM725.714 277.714c0 29.143-23.429 52.571-52.571 52.571s-52.571-23.429-52.571-52.571 23.429-52.571 52.571-52.571 52.571 23.429 52.571 52.571zM438.857 152c-64 0-201.143-5.143-258.857 17.714-20 8-34.857 17.714-50.286 33.143s-25.143 30.286-33.143 50.286c-22.857 57.714-17.714 194.857-17.714 258.857s-5.143 201.143 17.714 258.857c8 20 17.714 34.857 33.143 50.286s30.286 25.143 50.286 33.143c57.714 22.857 194.857 17.714 258.857 17.714s201.143 5.143 258.857-17.714c20-8 34.857-17.714 50.286-33.143s25.143-30.286 33.143-50.286c22.857-57.714 17.714-194.857 17.714-258.857s5.143-201.143-17.714-258.857c-8-20-17.714-34.857-33.143-50.286s-30.286-25.143-50.286-33.143c-57.714-22.857-194.857-17.714-258.857-17.714zM877.714 512c0 60.571 0.571 120.571-2.857 181.143-3.429 70.286-19.429 132.571-70.857 184s-113.714 67.429-184 70.857c-60.571 3.429-120.571 2.857-181.143 2.857s-120.571 0.571-181.143-2.857c-70.286-3.429-132.571-19.429-184-70.857s-67.429-113.714-70.857-184c-3.429-60.571-2.857-120.571-2.857-181.143s-0.571-120.571 2.857-181.143c3.429-70.286 19.429-132.571 70.857-184s113.714-67.429 184-70.857c60.571-3.429 120.571-2.857 181.143-2.857s120.571-0.571 181.143 2.857c70.286 3.429 132.571 19.429 184 70.857s67.429 113.714 70.857 184c3.429 60.571 2.857 120.571 2.857 181.143z"></path>
+                </svg>
+              </a>
               <svg viewBox="0 0 602.2582857142856 1024" className="home-icon14">
                 <path d="M548 6.857v150.857h-89.714c-70.286 0-83.429 33.714-83.429 82.286v108h167.429l-22.286 169.143h-145.143v433.714h-174.857v-433.714h-145.714v-169.143h145.714v-124.571c0-144.571 88.571-223.429 217.714-223.429 61.714 0 114.857 4.571 130.286 6.857z"></path>
               </svg>

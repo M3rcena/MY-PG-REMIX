e0b4b87479186cc40f2ef95b40e1af196ec4b422
M3rcena <102794864+M3rcena@users.noreply.github.com>
2024-01-10 20:02:25 +0200

    Added 404 Custom Page

---
+++ app/routes/$.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+
+import { Helmet } from "react-helmet";
+
+import errorStylesHref from "../styles/404.css";
+import { Link } from "@remix-run/react";
+
+export const links = () => {
+    return [
+        { rel: 'stylesheet', href: errorStylesHref },
+        { rel: 'stylesheet', href: "https://fonts.googleapis.com/css?family=Montserrat:700,900" }
+    ]
+}
+
+export function loader() {
+    return new Response("Not Found", {
+        status: 404,
+    });
+};
+
+export default function NotFoundPage() {
+    return (
+        <>
+            <div id="notfound">
+                <div className="notfound">
+                    <div className="notfound-404">
+                        <h1>404</h1>
+                        <h2>Page not found</h2>
+                    </div>
+                    <Link to="/">Homepage</Link>
+                </div>
+            </div>
+        </>
+    )
+}

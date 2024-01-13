dcd467afee8fb2db5433e9961e7f33daff2724df
M3rcena <102794864+M3rcena@users.noreply.github.com>
2024-01-13 18:50:27 +0200

    Temporary React  Files

---
+++ react/examples/Navbars/DefaultNavbar/DefaultNavbarLink.js
@@ -0,0 +1,53 @@
+import { Link } from "react-router-dom";
+
+// prop-types is a library for typechecking of props
+import PropTypes from "prop-types";
+
+// @mui material components
+import Icon from "@mui/material/Icon";
+
+import AdmBox from "components/AdmBox";
+import AdmTypography from "components/AdmTypography";
+
+function DefaultNavbarLink({ icon, name, route }) {
+  return (
+    <>
+      <AdmBox
+        component={Link}
+        to={route}
+        mx={1}
+        p={1}
+        display="flex"
+        alignItems="center"
+        sx={{ cursor: "pointer", userSelect: "none" }}
+      >
+        <Icon
+          sx={{
+            color: ({ palette: { white } }) => white.main,
+            verticalAlign: "middle",
+          }}
+        >
+          {icon}
+        </Icon>
+        <AdmTypography
+          variant="button"
+          fontWeight="regular"
+          color="white"
+          textTransform="capitalize"
+          sx={{ width: "100%", lineHeight: 0 }}
+        >
+          &nbsp;{name}
+        </AdmTypography>
+      </AdmBox>
+    </>
+  );
+}
+
+// Typechecking props for the DefaultNavbarLink
+DefaultNavbarLink.propTypes = {
+  icon: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  route: PropTypes.string.isRequired,
+};
+
+export default DefaultNavbarLink;

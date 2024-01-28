4bdedbc6e3b7ae85f47aa3bbaddb4dfd32be6103
M3rcena <102794864+M3rcena@users.noreply.github.com>
2024-01-28 17:41:46 +0200

    Add multiple database connections

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 const { Pool } = require('pg');
 require('dotenv').config();
 
-const pool = new Pool({
+const auth = new Pool({
   user: process.env.AUTH_USER,
   host: process.env.AUTH_HOST,
   database: process.env.AUTH_NAME,
@@ -9,4 +9,22 @@ const pool = new Pool({
   port: process.env.AUTH_PORT,
 });
 
-module.exports = pool;
+const map = new Pool({
+  user: process.env.MAP_USER,
+  host: process.env.MAP_HOST,
+  database: process.env.MAP_NAME,
+  password: process.env.MAP_PASSWORD,
+  port: process.env.MAP_PORT,
+});
+
+const user = new Pool({
+  user: process.env.USER_USER,
+  host: process.env.USER_HOST,
+  database: process.env.USER_NAME,
+  password: process.env.USER_PASSWORD,
+  port: process.env.USER_PORT,
+});
+
+module.exports = auth;
+module.exports = map;
+module.exports = user;

e6654becfd191acfa933e6c771cd42a8c4949083
M3rcena <102794864+M3rcena@users.noreply.github.com>
2024-02-14 17:41:12 +0200

    Uploaded to Host

--- app/routes/register.jsx
+++ app/routes/register.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
+import { PrismaClient } from '@prisma/client'
 
 import image from '../assets/img/Auth/signup-image.jpg'
 
 import authStylesHref from '../styles/auth.css'
+import { redirect } from 'react-router-dom'
 
 export const links = () => {
   return [
@@ -10,7 +12,43 @@ export const links = () => {
   ]
 }
 
+export async function action({ request }) {
+    const prisma = new PrismaClient();
+    if (request.method === 'POST') {
+      const formData = new URLSearchParams(await request.formData());
+      const name = formData.get('name');
+      const email = formData.get('email');
+      const password = formData.get('pass');
+      const rePassword = formData.get('re_pass');
+      const phone = formData.get('phone');
+  
+      if (name && email && password == rePassword && phone) {
+        try {
+          const auth = await prisma.user.create({
+            data: {
+              name: name.toString(),
+              email: email.toString(),
+              password: password.toString(),
+              phone: phone.toString(),
+            },
+          });
+  
+          return redirect('/account');
+        } catch (error) {
+          // Handle errors during user creation (e.g., email already exists)
+          console.error('Error creating user:', error);
+          return { error: 'Failed to create user' }; // Or redirect to an error page with more details
+        }
+      }
+  
+      return { error: 'Missing required fields' }; // Handle incomplete form data
+    }
+  
+    return redirect('/'); // Redirect to another page on non-POST requests
+  }
+
 export default function Register() {
+
   return (
     <>
     <section className="signup">
@@ -26,6 +64,10 @@ export default function Register() {
                             <div className="form-group">
                                 <label htmlFor="email"><i className="zmdi zmdi-email"></i></label>
                                 <input type="email" name="email" id="email" placeholder="Your Email"/>
+                            </div>
+                            <div className="form-group">
+                                <label htmlFor="phone"><i className="zmdi zmdi-phone"></i></label>
+                                <input type="text" name="phone" id="phone" placeholder="Your Phone"/>
                             </div>
                             <div className="form-group">
                                 <label htmlFor="pass"><i className="zmdi zmdi-lock"></i></label>

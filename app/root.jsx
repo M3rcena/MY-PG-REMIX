dfce042c1fba9315afe8a03daae6f81e6f9405e5
M3rcena <michaelfarmakis2007@gmail.com>
2024-02-15 14:44:16 +0000

    Removed Features Causing Errors

--- app/root.jsx
+++ app/root.jsx
@@ -68,92 +68,10 @@ export default function App() {
           gtag('config', 'G-S1MWV8T99P');
   </script>*/}
 
-
-        <link rel='stylesheet' id='e-animations-css'
-          href='./assets/features/wp-content/plugins/elementor/assets/lib/animations/animations.min.css' type='text/css'
-          media='all' />
-
         <script
           data-section-id="navbar"
           src="https://unpkg.com/@teleporthq/teleport-custom-scripts"
         ></script>
-
-        <script type="text/javascript" id="eael-general-js-extra"
-          src="./assets/features/js/localize.jsx">
-        </script>
-
-        <script type="text/javascript"
-          src="./assets/features/wp-content/plugins/essential-addons-for-elementor-lite/assets/front-end/js/view/general.min.jsx"
-          id="eael-general-js"></script>
-        <script type="text/javascript" src="./assets/features/wp-content/uploads/essential-addons-elementor/eael-1261.jsx"
-          id="eael-1261-js"></script>
-        <script type="text/javascript" src="./assets/features/wp-content/uploads/essential-addons-elementor/eael-1264.jsx"
-          id="eael-1264-js"></script>
-        <script type="text/javascript"
-          src="./assets/features/wp-content/plugins/easyjobs/public/assets/vendor/owl.carousel.min.jsx"
-          id="easyjobsowl-js"></script>
-        <script type="text/javascript"
-          src="./assets/features/wp-content/plugins/easyjobs/public/assets/dist/js/easyjobs-public.min.jsx"
-          id="easyjobs-js"></script>
-
-        <script type="text/javascript" id="rocket-browser-checker-js-after"
-          src="./assets/features/js/RocketBrowserChecker.jsx">
-        </script>
-        <script type="text/javascript" id="rocket-preload-links-js-extra"
-          src="./assets/features/js/RocketPreloadLinksConfig.jsx">
-        </script>
-        <script type="text/javascript" id="rocket-preload-links-js-after"
-          src="./assets/features/js/RocketPreloadLinks.jsx">
-        </script>
-        <script type="text/javascript" id="flexia-navigation-js-extra"
-          src="./assets/features/js/settings.jsx">
-        </script>
-
-        <script type="text/javascript"
-          src="./assets/features/wp-content/themes/flexia/framework/assets/site/js/flexia-scripts.min.jsx?ver=6.4.2"
-          id="flexia-navigation-js"></script>
-        <script type="text/javascript"
-          src="./assets/features/wp-content/uploads/essential-addons-elementor/eael-1272.jsx?ver=1629781368"
-          id="eael-1272-js"></script>
-        <script type="text/javascript"
-          src="./assets/features/wp-content/plugins/elementor-pro/assets/js/webpack-pro.runtime.min.jsx?ver=3.18.3"
-          id="elementor-pro-webpack-runtime-js"></script>
-        <script type="text/javascript"
-          src="./assets/features/wp-content/plugins/elementor/assets/js/webpack.runtime.min.jsx?ver=3.18.3"
-          id="elementor-webpack-runtime-js"></script>
-        <script type="text/javascript"
-          src="./assets/features/wp-content/plugins/elementor/assets/js/frontend-modules.min.jsx?ver=3.18.3"
-          id="elementor-frontend-modules-js"></script>
-        <script type="text/javascript" src="./assets/features/wp-includes/js/dist/vendor/wp-polyfill-inert.min.jsx?ver=3.1.2"
-          id="wp-polyfill-inert-js"></script>
-        <script type="text/javascript" src="./assets/features/wp-includes/js/dist/vendor/regenerator-runtime.min.jsx"
-          id="regenerator-runtime-js"></script>
-        <script type="text/javascript" src="./assets/features/wp-includes/js/dist/vendor/wp-polyfill.min.jsx"
-          id="wp-polyfill-js"></script>
-        <script type="text/javascript" src="./assets/features/wp-includes/js/dist/hooks.min.jsx" id="wp-hooks-js"></script>
-        <script type="text/javascript" src="./assets/features/wp-includes/js/dist/i18n.min.jsx" id="wp-i18n-js"></script>
-
-        <script type="text/javascript" id="wp-i18n-js-after"
-          src="./assets/features/js/i18n.jsx">
-        </script>
-
-        <script type="text/javascript" id="elementor-pro-frontend-js-before" src="./assets/features/js/ElementorProFrontendConfig.jsx">
-        </script>
-        <script type="text/javascript" src="./assets/features/wp-content/plugins/elementor-pro/assets/js/frontend.min.jsx"
-          id="elementor-pro-frontend-js"></script>
-        <script type="text/javascript" src="./assets/features/wp-content/plugins/elementor/assets/lib/waypoints/waypoints.min.jsx"
-          id="elementor-waypoints-js"></script>
-        <script type="text/javascript" src="./assets/features/wp-includes/js/jquery/ui/core.min.jsx"
-          id="jquery-ui-core-js"></script>
-        <script type="text/javascript" id="elementor-frontend-js-before"
-          src="./assets/features/js/elementorFrontendConfig.jsx">
-        </script>
-        <script type="text/javascript" src="./assets/features/wp-content/plugins/elementor/assets/js/frontend.min.jsx"
-          id="elementor-frontend-js"></script>
-        <script type="text/javascript"
-          src="./assets/features/wp-content/plugins/elementor-pro/assets/js/elements-handlers.min.jsx"
-          id="pro-elements-handlers-js"></script>
-
 
         <script src="https://kit.fontawesome.com/a1a8c654c5.js" crossOrigin="anonymous"></script>
         <Scripts />

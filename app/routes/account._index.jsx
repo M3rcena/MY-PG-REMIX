39a459a4153b34bfed730a2a1d68baaa7c085eac
M3rcena <102794864+M3rcena@users.noreply.github.com>
2024-02-27 15:32:15 +0200

    Add session management and user authentication to Account route

--- app/routes/account._index.jsx
+++ app/routes/account._index.jsx
@@ -1,4 +1,49 @@
-import React from 'react'
+// Import React and Remix Packages
+import React from 'react';
+import { redirect } from '@remix-run/react';
+
+// Import Prisma Client
+import { PrismaClient } from '@prisma/client';
+
+// Import Session Storage
+import { getSession, commitSession } from '~/session.server';
+
+export async function loader(request) {
+  // Get User Information
+  const session = await getSession(request.headers.get('Cookie'));
+  const userId = session?.get('userId');
+  console.log(userId)
+
+  // Redirect to Login Page if User is not Logged In
+  if (!userId) {
+    return redirect('/login');
+  };
+
+  // Get User Information
+  let user = null;
+  if (userId) {
+    const prisma = new PrismaClient();
+    user = await prisma.user.findUnique({
+      where: { id: userId },
+      select: {
+        id: true,
+        email: true,
+        role: true,
+      }
+    });
+    await prisma.$disconnect();
+  };
+
+  if (user && !session.get("role")) {
+    await commitSession(request, session.set("role", user.role));
+  };
+
+  if (user?.role === "guest") {
+    return redirect("/");
+  };
+
+  return { data: "This is protected content accessible with a non-guest session.", user };
+}
 
 export default function Account() {
   return (
---
+++ app/session.server.jsx
@@ -0,0 +1,15 @@
+import { createCookieSessionStorage } from "@remix-run/node";
+
+export const sessionStorage = createCookieSessionStorage({
+  cookie: {
+    name: "__session",
+    sameSite: "lax",
+    path: "/",
+    httpOnly: true,
+    secrets: ["s3cr3t"],
+    secure: process.env.NODE_ENV === "production",
+  },
+  dir: "/app/storedSessions"
+});
+
+export const { getSession, commitSession, destroySession } = sessionStorage;
